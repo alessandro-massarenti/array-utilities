30200d6424827819cd2ddb38513fd0c2c51fd554
AndreaBreggion <49444060+AndreaBreggion@users.noreply.github.com>
2019-04-09 16:51:28 +0200

    added function PrintHAlt
    Added function that prints H-slices of an array that is not completely filled, based on its dimensions and number of elements

--- scorri_hfette.cpp
+++ scorri_hfette.cpp
@@ -1,4 +1,4 @@
-void scorriH(int x[], int dim1, int dim2, int dim3) {
+void scorriH(int x[], int dim1, int dim2, int dim3) {     //works if the array is completely filled
     for (int i = 0; i < dim2; i++) {
         cout << "fetta" << i << endl;
         for (int j = 0; j < dim1; j++) {
@@ -12,3 +12,24 @@ void scorriH(int x[], int dim1, int dim2, int dim3) {
         cout << endl;
     }
 }
+
+void PrintHAlt(int * x,int n_ele, int dim2,int dim3)
+{
+    int z = n_ele/(dim2*dim3); //find how many z levels are filled
+    int Unfilled_elements = n_ele%(dim2*dim3); //find how many elements are in the last z level
+    int last_level_rows = Unfilled_elements / dim3;
+    int spare = Unfilled_elements % dim3;
+    for (int i = 0 ; i<dim2;i++)
+    {
+       int El = dim3*z;
+       if(i<last_level_rows) El+=dim3;
+       if(i==last_level_rows) El+=spare;
+        cout<<"slice "<< i <<endl;
+        for(int c = 0 ; c<El;c++)
+        {
+            if((c%dim3)==0) cout<<"row "<< c/dim3<<": ";
+        cout<< x[i*dim3+(c/dim3)*(dim2*dim3)+c%dim3]<<" ";
+            if((c%dim3)==dim3-1) cout<<endl;
+        }
+cout<<endl;
+    }
