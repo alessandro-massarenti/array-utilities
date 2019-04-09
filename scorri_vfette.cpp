070569d74e1497ecbf4d8e25cf9e20c5f3515576
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-04-09 16:27:01 +0200

    Update scorri_vfette.cpp

--- scorri_vfette.cpp
+++ scorri_vfette.cpp
@@ -1,6 +1,14 @@
+//------------------------------------------------------------------
+//Questa funzione stampa per fette verticali un array a 3 dimensioni
+//------------------------------------------------------------------
+
+// PRE = (dim1 dim2 e dim3 denotano le dimensioni di un array sono completamente pieno)
 void scorriV(int x[],int dim1,int dim2,int dim3){
+	
+	//PRE=(dim3 è esattamente il numero di elementi in una singola riga)
 	for(int i=0;i<dim3; i++){
 		cout << "v-fetta" << i << endl;
+		//PRE=(dim1 è esattamente il numero di strati, dim2 è esattamente il numero di righe presenti in uno strato)
 		for(int k=0;k<dim1*dim2; k++){
 			cout << *(x+(dim3*k)+i) << " ";
 		}
